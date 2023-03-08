ab11efd082b9c80d4fa8a59d9fe493cef893e6e1
a.vitsiuk <a.vitsiuk@affidaty.io>
2023-03-08 03:45:09 +0100

    added demo animation

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,5 +1,7 @@
 import Game from './gameApp';
 
+import { testWorld1 } from './testInitArray';
+
 const canvasElemId = 'gameCanvas';
 const runBtnElemId = 'runBtn';
 const haltBtnElemId = 'haltBtn';
@@ -8,74 +10,38 @@ const dbgTglBtnElemId = 'dbgTglBtn';
 
 const canvasWidthPx = 1000;
 const canvasHeightPx = 600;
-const worldDims = { x: 7, y: 7, z: 5 };
-const initArray: number[] = [
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-
-    1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 0, 0, 1,
-    1, 0, 1, 1, 1, 0, 1,
-    1, 0, 1, 0, 1, 0, 1,
-    1, 0, 1, 1, 1, 0, 1,
-    1, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1,
-
-    1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 1, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1,
-
-    1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 0, 0, 1,
-    1, 0, 1, 1, 1, 0, 1,
-    1, 0, 1, 0, 1, 0, 1,
-    1, 0, 1, 1, 1, 0, 1,
-    1, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1,
-
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1,
-];
 
 async function main() {
     const game = new Game(canvasElemId, { x: canvasWidthPx, y: canvasHeightPx })
-        .setWorldSize(worldDims)
-        .initFromArray(initArray)
-        .setRenderedSubfield(
-            { x: 0, y: 0, z: 0 },
-            { x: 7, y: 7, z: 1 },
-        )
+        .setWorldSize(testWorld1.dims)
+        .initFromArray(testWorld1.initArray)
         .showDebug({ x: 0, y: canvasHeightPx - 60 })
         .showCtrl({ x: 120, y: canvasHeightPx - 60 });
 
-    // cycle layers
-    setInterval(
-        () => {
-            const sf = game.renderedSubfield;
-            sf.origin = {
-                x: sf.origin.x,
-                y: sf.origin.y,
-                z: (sf.origin.z + 1) % game.world.fieldDims.z,
-            };
-            game.setRenderedSubfield(sf.origin, sf.dims);
-            console.log(`[${sf.origin.x};${sf.origin.y};${sf.origin.z}],[${sf.dims.x};${sf.dims.y};${sf.dims.z}]`);
-        },
-        1000,
-    );
+    setTimeout(() => {
+        game.setRenderedSubfield(
+            { x: 0, y: 0, z: 0 },
+            { x: testWorld1.dims.x - 1, y: testWorld1.dims.y, z: testWorld1.dims.z },
+        );
+        setTimeout(() => {
+            game.setRenderedSubfield(
+                { x: 0, y: 0, z: 0 },
+                { x: testWorld1.dims.x, y: testWorld1.dims.y, z: 1 },
+            );
+            setInterval(() => {
+                const sf = game.renderedSubfield;
+                sf.origin = {
+                    x: sf.origin.x,
+                    y: sf.origin.y,
+                    z: (sf.origin.z + 1) % game.world.fieldDims.z,
+                };
+                game.setRenderedSubfield(sf.origin, sf.dims);
+                console.log(
+                    `[${sf.origin.x};${sf.origin.y};${sf.origin.z}],[${sf.dims.x};${sf.dims.y};${sf.dims.z}]`,
+                );
+            }, 1000);
+        }, 2000);
+    }, 3000);
     document.getElementById(runBtnElemId)?.addEventListener('click', () => {
         game.run();
     });
